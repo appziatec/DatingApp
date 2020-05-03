41f8b315666a25627c291691c2f29c6e943aceae
appziatec <appziatechnologies@gmail.com>
2020-05-03 22:01:46 +0530

    initial Commit

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [StringLength(12, MinimumLength = 4, ErrorMessage = "Password minimum length must be 4")]
         public string Password { get; set; }
     }
 }
